87f07529f9bbe4b6c4f0949859b9f091bfd2aca4
Hakan Olgun <77194816+hakanolgun@users.noreply.github.com>
2021-09-13 16:22:14 +0300

    Update sqlOdevNo5.sql

--- sqlOdevNo5.sql
+++ sqlOdevNo5.sql
@@ -6,3 +6,14 @@ FROM FILM
 WHERE TITLE like '%n'
 order by length desc
 limit 5;
+
+-- 2. film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci 5 filmi sıralayınız.
+
+SELECT TITLE,
+	LENGTH
+FROM FILM
+WHERE TITLE like '%n'
+order by length asc
+offset 5
+limit 5;
+
