6342675faeeecb85f46e4650253a0ba1f0df05bb
Hakan Olgun <77194816+hakanolgun@users.noreply.github.com>
2021-09-13 10:14:20 +0300

    Update sqlOdevNo3.sql

--- sqlOdevNo3.sql
+++ sqlOdevNo3.sql
@@ -14,7 +14,7 @@ WHERE country LIKE '%n' AND Length(country) > 5;
 
 SELECT TITLE
 FROM FILM
-WHERE TITLE ILIKE '%T%T%T%T';
+WHERE TITLE ILIKE '%T%T%T%T%';
 
 
 -- 4. film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
