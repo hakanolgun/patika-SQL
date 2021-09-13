f7a6061efc8d26c280cab4700cdae0b7c97a68a8
Hakan Olgun <77194816+hakanolgun@users.noreply.github.com>
2021-09-13 17:38:50 +0300

    Update sqlOdevNo6.sql

--- sqlOdevNo6.sql
+++ sqlOdevNo6.sql
@@ -2,3 +2,21 @@
 
 SELECT AVG(RENTAL_RATE) FROM FILM;
 
+-- 2. film tablosunda bulunan filmlerden kaçtanesi 'C' karekteri ile başlar?
+
+SELECT COUNT(*)
+FROM FILM
+WHERE TITLE like 'C%';
+
+-- 3. film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
+
+SELECT MAX(LENGTH)
+FROM FILM
+WHERE RENTAL_RATE = 0.99;
+
+
+-- 4. film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+
+SELECT COUNT(DISTINCT REPLACEMENT_COST)
+FROM FILM
+WHERE LENGTH > 150;
