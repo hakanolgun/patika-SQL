bd97d4b19513ffb65f658833a18efba5082bed5c
Hakan Olgun <77194816+hakanolgun@users.noreply.github.com>
2021-09-13 16:24:29 +0300

    Update sqlOdevNo5.sql

--- sqlOdevNo5.sql
+++ sqlOdevNo5.sql
@@ -17,3 +17,9 @@ order by length asc
 offset 5
 limit 5;
 
+-- 3. customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
+
+select last_name, store_id from customer
+where store_id = '1'
+order by last_name desc
+limit 4;
