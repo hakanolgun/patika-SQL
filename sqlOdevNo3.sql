6d125055dc66f87bf1a13e918e2fcbceba4b2d18
Hakan Olgun <77194816+hakanolgun@users.noreply.github.com>
2021-09-13 09:18:31 +0300

    Update sqlOdevNo3.sql

--- sqlOdevNo3.sql
+++ sqlOdevNo3.sql
@@ -2,3 +2,8 @@
 SELECT * from country
 WHERE country LIKE 'A%a';
 
+
+-- 2.country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
+SELECT * from country
+WHERE country LIKE '%n' AND Length(country) > 5;
+
