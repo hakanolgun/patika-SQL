a15c2629f7fc8070326c2c314d7569776e25b9ce
Hakan Olgun <77194816+hakanolgun@users.noreply.github.com>
2021-09-13 09:46:03 +0300

    Update sqlOdevNo4.sql

--- sqlOdevNo4.sql
+++ sqlOdevNo4.sql
@@ -8,3 +8,13 @@ FROM FILM;
 
 SELECT COUNT (DISTINCT REPLACEMENT_COST)
 FROM FILM;
+
+-- 3. film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
+
+SELECT COUNT(*)
+FROM FILM
+WHERE TITLE ilike 'T%'
+	AND RATING = 'G';
+
+
+-- 4.
