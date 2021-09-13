39f37dd57ec9c0dd511f48ab5d136950cb7a2292
Hakan Olgun <77194816+hakanolgun@users.noreply.github.com>
2021-09-13 09:26:02 +0300

    Update sqlOdevNo3.sql

--- sqlOdevNo3.sql
+++ sqlOdevNo3.sql
@@ -1,9 +1,26 @@
 -- 1. country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
+
 SELECT * from country
 WHERE country LIKE 'A%a';
 
 
--- 2.country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
+-- 2. country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
+
 SELECT * from country
 WHERE country LIKE '%n' AND Length(country) > 5;
 
+
+-- 3. film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
+
+SELECT TITLE
+FROM FILM
+WHERE TITLE ILIKE '%T%T%T%T';
+
+
+-- 4. film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
+
+SELECT *
+FROM FILM
+WHERE TITLE like 'C%'
+	AND LENGTH > 90
+	AND RENTAL_RATE = 2.99;
