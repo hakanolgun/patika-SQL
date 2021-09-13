8e00674869d82be41b6da9d34381739b9d6fd998
Hakan Olgun <77194816+hakanolgun@users.noreply.github.com>
2021-09-13 09:15:16 +0300

    Create sqlOdevNo3.sql

---
+++ sqlOdevNo3.sql
@@ -0,0 +1,4 @@
+-- 1. country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
+SELECT * from country
+WHERE country LIKE 'A%a';
+
