48c117f8d3312b18d9ed7c9273afdc2668393a1d
Hakan Olgun <77194816+hakanolgun@users.noreply.github.com>
2021-09-14 10:29:53 +0300

    Update sqlOdevNo8.sql

--- sqlOdevNo8.sql
+++ sqlOdevNo8.sql
@@ -1,7 +1,14 @@
 -- 1.  test veritabanınızda employee isimli sütun bilgileri id(INTEGER), name VARCHAR(50), birthday DATE, email VARCHAR(100) olan bir tablo oluşturalım.
 
+CREATE TABLE EMPLOYEE (
+	ID INTEGER PRIMARY KEY,
+	NAME VARCHAR(50) NOT NULL,
+	BIRTHDAY DATE,
+	EMAIL VARCHAR(100));
+
 
 -- 2.  Oluşturduğumuz employee tablosuna 'Mockaroo' servisini kullanarak 50 adet veri ekleyelim.
+
 
 
 -- 3.  Sütunların her birine göre diğer sütunları güncelleyecek 5 adet UPDATE işlemi yapalım.
