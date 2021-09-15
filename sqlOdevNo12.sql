b0bc49105f4c4455762049396778b665014ec561
Hakan Olgun <77194816+hakanolgun@users.noreply.github.com>
2021-09-15 12:53:06 +0300

    Update sqlOdevNo12.sql

--- sqlOdevNo12.sql
+++ sqlOdevNo12.sql
@@ -8,12 +8,22 @@ where length > (
 
 -- 2.    film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
 
+select count(title) from film
+where rental_rate = (
+select max(rental_rate) from film
+);
 
 
 
 -- 3.    film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
 
-
+select title,rental_rate,replacement_cost from film
+order by rental_rate, replacement_cost;
 
 
 -- 4.    payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+
+select customer_id, count(customer_id) as val
+from payment
+group by customer_id
+order by val desc;
