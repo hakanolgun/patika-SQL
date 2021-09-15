b4ddcbe328543beba061b0731f1671acba340a2e
Hakan Olgun <77194816+hakanolgun@users.noreply.github.com>
2021-09-15 09:49:23 +0300

    Update sqlOdevNo9.sql

--- sqlOdevNo9.sql
+++ sqlOdevNo9.sql
@@ -7,7 +7,8 @@ inner join country on city.country_id = country.country_id;
 
 -- 2.    customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
 
-
+select payment_id, first_name, last_name from payment
+inner join customer on customer.customer_id = payment.customer_id;
 
 
 -- 3.    customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
